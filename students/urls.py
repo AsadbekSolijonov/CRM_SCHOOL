3503a07a1f8a24cc3ce6f4287e6f1599669c7406
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-07-18 10:22:34 +0500

    dashboard create

--- students/urls.py
+++ students/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from students import views
 
 urlpatterns = [
-
+    path('', views.dashboard, name='dashboard'),
+    path('employee/', views.employee, name='employee'),
+    path('groups/', views.groups, name='groups'),
+    path('events/', views.events, name='events'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, 'dashboard_components.html')
+
+
+def employee(request):
+    return render(request, 'employee.html')
+
+
+def groups(request):
+    return render(request, 'dash_group.html')
+
+
+def events(request):
+    return render(request, 'dash_event.html')
